f019e8bcc3fc48bd5ef21b03a4b64906808f7454
Jeremy Grosser <jeremy@synack.me>
2020-08-31 03:33:27 +0000

    rename to autoinstall.img

--- build_ami.sh
+++ build_ami.sh
@@ -3,8 +3,8 @@ set -x
 
 TARGET=/dev/rsd1c
 
-if [ ! -f install.fs ]; then
-	ftp -o install.fs https://cdn.openbsd.org/pub/OpenBSD/6.7/amd64/install67.fs
+if [ ! -f install67.img ]; then
+	ftp -o install67.img https://cdn.openbsd.org/pub/OpenBSD/6.7/amd64/install67.fs
 fi
 if [ ! -f bsd.rd ]; then
 	ftp https://cdn.openbsd.org/pub/OpenBSD/6.7/amd64/bsd.rd
@@ -27,39 +27,39 @@ rdsetroot bsd.rd disk.fs
 rm disk.fs
 
 # 512MB
-dd if=/dev/zero of=new.fs bs=512 count=1048600
-vnconfig vnd2 new.fs
+dd if=/dev/zero of=autoinstall.img bs=512 count=1048600
+vnconfig vnd2 autoinstall.img
 fdisk -yi -b 960 -f /usr/mdec/mbr vnd2
 disklabel -w vnd2 install512
-newfs -m 0 -o space -S 512 -s 1048512 /dev/rvnd2a
-mkdir new
-mount /dev/vnd2a new
+autoinstallfs -m 0 -o space -S 512 -s 1048512 /dev/rvnd2a
+mkdir autoinstall
+mount /dev/vnd2a autoinstall
 
-vnconfig vnd0 install.fs
+vnconfig vnd0 install67.img
 mkdir install
 mount /dev/vnd0a install
 
-cp -r install/{6.7,boot,etc,bsd} new/
-cp bsd.rd new/
-cp boot.conf new/etc/
-cp bsd.mp new/6.7/amd64/bsd.mp
-tar czvf new/6.7/amd64/site67.tgz -C site .
-ls -l new/6.7/amd64/ >new/6.7/amd64/index.txt
-installboot -v -r new vnd2 /usr/mdec/biosboot new/boot
+cp -r install/{6.7,boot,etc,bsd} autoinstall/
+cp bsd.rd autoinstall/
+cp boot.conf autoinstall/etc/
+cp bsd.mp autoinstall/6.7/amd64/bsd.mp
+tar czvf autoinstall/6.7/amd64/site67.tgz -C site .
+ls -l autoinstall/6.7/amd64/ >autoinstall/6.7/amd64/index.txt
+installboot -v -r autoinstall vnd2 /usr/mdec/biosboot autoinstall/boot
 
 umount install
 vnconfig -u vnd0
 rm -rf install
 
-umount new
+umount autoinstall
 vnconfig -u vnd2
-rm -rf new
+rm -rf autoinstall
 
 qemu-system-x86_64 \
     -nographic \
     -serial mon:stdio \
     -netdev user,id=net0 -device virtio-net,netdev=net0 \
-    -hda new.fs \
+    -hda autoinstall.img \
     -hdb ${TARGET} \
     -m 512 \
     -no-reboot
